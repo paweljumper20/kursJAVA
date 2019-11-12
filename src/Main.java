602bce26ea68016db35d1554e5f8009b848510ad
paweljumper20 <57679780+paweljumper20@users.noreply.github.com>
2019-11-12 19:55:20 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Druga linia ");
+        
+        
+         System.out.println("trzecia linia ");
+        
 
     }
 }
